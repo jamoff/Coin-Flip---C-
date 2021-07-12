4986a36d6c06f96092d0a65a9991cc7b1c5e21fe
jamoff <87047820+jamoff@users.noreply.github.com>
2021-07-12 08:00:32 -0400

    Update coinflip.cpp

--- coinflip.cpp
+++ coinflip.cpp
@@ -1,1 +1,45 @@
 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int coinFlip (){
+
+int flip = (rand()%2)+ 1 ;
+
+return flip;
+
+}
+
+int main()
+
+{
+
+int counter = 0; 
+int flip = 0;
+
+cout << "How many coin tosses would you like?" << endl; 
+cin >> counter;
+
+srand (time(NULL));
+
+for (int i = 1; i <= counter; i++) {
+ flip = coinFlip();
+
+if (flip == 1) {
+
+cout << "Heads" << endl;
+
+}
+
+else {
+
+cout << "Tails" << endl;
+
+}
+
+}
+
+}
